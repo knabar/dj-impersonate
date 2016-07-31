b5c9af750c5f02db276273da682c038070cd905a
Andreas Knab <knabar@gmail.com>
2016-07-31 18:54:17 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+setuptools.setup(
+    name='dj-impersonate',
+    version='0.1',
+    url='http://github.com/knabar/dj-impersonate',
+    author='Andreas Knab',
+    author_email='knabar@gmail.com',
+    description='Allow users to work under another user account',
+    long_description='Allow users to work under another user account',
+    platforms=['All'],
+    license='MIT',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+    ],
+    dependency_links=[
+    ],
+)
